861b72570683accfab79b0b69bef6e5d08aa313d
young <hyangyoungs@gmail.com>
2024-07-27 00:21:24 -0500

    update: portfolio with mouse hover effect

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // menu splitting
-$(function (){
+$(function () {
     Splitting();
 })
 
 // header scroll
-$(function (){
+$(function () {
     var prevScrollTop = 0;
-    document.addEventListener("scroll", function (){
+    document.addEventListener("scroll", function () {
         var nowScrollTop = $(window).scrollTop();
 
-        if(nowScrollTop > prevScrollTop) {
+        if (nowScrollTop > prevScrollTop) {
             $('header').addClass('active');
         } else {
             $('header').removeClass('active');
@@ -19,7 +19,7 @@ $(function (){
 });
 
 // animate scroll
-$(function (){
+$(function () {
     $(".animate").scrolla({
         mobile: true,
         once: false,
@@ -27,7 +27,7 @@ $(function (){
 });
 
 // section pinning with ScrollTrigger
-$(function (){
+$(function () {
     gsap.registerPlugin(ScrollTrigger);
 
     const sections = gsap.utils.toArray('section');
@@ -37,32 +37,64 @@ $(function (){
             start: 'top top',
             pin: true,
             pinSpacing: false,
-            // markers: true
+            markers: true
         });
     });
 
     ScrollTrigger.create({
         snap: 1 / (sections.length - 1)
     });
+
+    ScrollTrigger.create({
+        trigger: "#project",
+        start: "top top",
+        pin: true,
+        pinSpacing: true,
+        markers: true
+    });
 });
 
-// background animation with ScrollTrigger
+
 $(function () {
     gsap.registerPlugin(ScrollTrigger);
 
-    const frameCount = 21;
-    const offsetValue = 100;
+    let activeImg;
 
-    gsap.to('.stage', {
-        backgroundPosition: `${-offsetValue * frameCount * 2}px center`,
-        ease: `steps(${frameCount})`,
-        scrollTrigger: {
-            trigger: '.character',
-            start: 'top top',
-            end: `+=${frameCount * offsetValue}`,
-            pin: true,
-            scrub: true,
-            markers: true
+    gsap.utils.toArray('.portfolio ul li a').forEach((elem) => {
+        let image = elem.querySelector('img.fadeImg');
+
+        const align = (e) => {
+            gsap.to(image, {
+                x: e.clientX,
+                y: e.clientY,
+                duration: 0.5,
+                ease: "elastic"
+            });
         }
+
+        const startPoint = () => document.addEventListener('mousemove', align);
+        const stopPoint = () => document.removeEventListener('mousemove', align);
+
+        const fade = gsap.to(image, {autoAlpha: 0.8, ease: 'none', paused: true});
+
+        elem.addEventListener('mouseenter', (e) => {
+            fade.play();
+            startPoint();
+
+            if (activeImg) {
+                gsap.set(image, {
+                    x: gsap.getProperty(activeImg, "x"),
+                    y: gsap.getProperty(activeImg, "y"),
+                });
+            }
+            activeImg = image;
+
+            align(e); // 마우스 위치를 즉시 맞추기 위해 호출
+        });
+
+        elem.addEventListener('mouseleave', () => {
+            fade.reverse();
+            stopPoint();
+        });
     });
 });
